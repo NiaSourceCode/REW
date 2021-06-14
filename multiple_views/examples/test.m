12b1d80819b1b93c92def0e5c8bbf5a754c34e84
niabie <2104934038@qq.com>
2021-06-14 12:59:08 +0800

    temp: 添加注释

--- multiple_views/examples/test.m
+++ multiple_views/examples/test.m
@@ -2,13 +2,14 @@
 % %{
 % imfolder = 'images/test';
 % imfolder = 'images/fcase_foundation';
-imfolder = '/home/lynx/fuck_mount/opencv/workspace/test_result/paper/29';
-im_n = 3; % 图片数
+imfolder = '/home/lynx/study/stitch/dataset/z_test';
+% imfolder = '/home/lynx/fuck_mount/opencv/workspace/test_result/paper/16';
+im_n = 2; % 图片数
 imfile = cell(im_n,1);
 
 imfile{1} = [imfolder '/' '1.jpg']
 imfile{2} = [imfolder '/' '2.jpg']
-imfile{3} = [imfolder '/' '3.jpg']
+% imfile{3} = [imfolder '/' '3.jpg']
 % imfile{4} = [imfolder '/' '4.jpg']
 % imfile{5} = [imfolder '/' '5.jpg']
 % imfile{6} = [imfolder '/' '6.jpg']
@@ -22,8 +23,12 @@ end
 
 edge_list = [
     1,2;
-    2,3;
+    % 2,3;
     % 3,4;
+    % 4,5;
+    % 5,6;
+    % 6,7;
+    % 7,8;
 ];
 
 imsize = zeros(im_n,3);
@@ -38,7 +43,7 @@ for ii = 1:im_n
     end
 end
 
-mosaic = REW_mosaic( im, edge_list, 0, 'persp', 0, imfolder );
+mosaic = REW_mosaic( im, edge_list, 1, 'persp', 0, imfolder );
 % parm2: edge_list, 可以为[]
 % parm3: ref图片的index, 如果为0则没有参考图像, 倾向于全局
 % parm4: 'persp'(正常) 或 'equi'(球面透视)
